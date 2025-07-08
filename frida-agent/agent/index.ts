92032b1ea7ca91178783d69ad02022df3d9217f8
Thanasis Petsas <thanpetsas@gmail.com>
2025-07-08 05:51:12 -0700

    updated deprecated APIs

--- frida-agent/agent/index.ts
+++ frida-agent/agent/index.ts
@@ -1,39 +1,55 @@
+import Java from "frida-java-bridge";
+import { dumpActor, dumpSDK, dumpActorName } from "./dump";
+import { GNames_offset, GWorld_offset, GUObjectArray_offset } from "./offset";
+
 var GWorld: NativePointer = ptr(0);
 var GNames: NativePointer = ptr(0);
 var GUObjectArray: NativePointer = ptr(0);
 var ue4: NativePointer | null = ptr(0);
-import { dumpActor, dumpSDK, dumpActorName } from "./dump.js";
-import { GNames_offset, GWorld_offset, GUObjectArray_offset } from "./offset.js"
 
 function init() {
-    ue4 = Module.findBaseAddress("libUE4.so");
-    if (ue4 != null) {
-        GWorld = ue4.add(GWorld_offset).readPointer()
-        GNames = ue4.add(GNames_offset)
-        GUObjectArray = ue4.add(GUObjectArray_offset)
+    try {
+        ue4 = Process.getModuleByName("libUE4.so")!.base;
+        if (ue4 != null) {
+            GWorld = ue4.add(GWorld_offset).readPointer()
+            GNames = ue4.add(GNames_offset)
+            GUObjectArray = ue4.add(GUObjectArray_offset)
+        }
+    } catch (e: unknown) {
+        console.warn(
+            `[!] Could not find module libUE4.so, exc: ${
+                (e as Error)?.stack?.toString() as string
+            }`
+        );
     }
 }
 
 function DumpSDK() {
-    Java.perform(function () {
+    if (!Java.available)
+        throw new Error(`Java is unavailable, exiting...`);
+    Java.perform(() => {
         init();
         dumpSDK(GNames, GUObjectArray);
-    })
+    });
 }
 
 
 function DumpActor() {
-    Java.perform(function () {
+    if (!Java.available)
+        throw new Error(`Java is unavailable, exiting...`);
+    Java.perform(() => {
         init();
         dumpActor(GNames, GUObjectArray);
-    })
+    });
 }
 
 function DumpActorName() {
-    Java.perform(function () {
+    if (!Java.available)
+        throw new Error(`Java is unavailable, exiting...`);
+    Java.perform(() => {
         init();
         dumpActorName(GWorld, GNames, false);
-    })
+    });
 }
 
 rpc.exports = {
